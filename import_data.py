6abfec5c7c738427b90aa9f81842d48ad8074e74
unidadconsciente <enriqueasv93@gmail.com>
2026-02-18 15:56:59 -0600

    Update import_data.py

--- import_data.py
+++ import_data.py
@@ -1,15 +1,11 @@
 import streamlit as st
-import pandas as pd
-import gspread
-from google.oauth2.service_account import Credentials
-import plotly.graph_objects as go
 
 # --- CONFIGURACIÓN MAESTRA ---
 SHEET_ID = "1MTuLBBk_-l5QRffFEktVZG_6pFwvl4SfshKTgVkd09s"
-MIN_OBS = 3  # Umbral de suficiencia de observación
+MIN_OBS = 3  
 PASSWORD_CEO = "Liderazgo2026"
 
-# --- MAPEO DE DOMINIOS (ADN HOGAN) ---
+# --- MAPEO DE DOMINIOS ---
 MAPEO_HOGAN = {
     "Self-Management": list(range(1, 11)),
     "Relationship Management": list(range(11, 26)),
